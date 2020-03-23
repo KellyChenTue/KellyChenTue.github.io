d4070a32c7ad3efe3b6d953970f38a0ea0d5155d
KellyChenTue <11gives1111@gmail.com>
2020-03-23 12:24:32 +0100

    clock test final update

--- clock.js
+++ clock.js
@@ -1,21 +1,16 @@
 var id;
-
-function getUserName() {
-    id = document.getElementById('nameField').value;
+/* Get User ID*/
+function getUserId() {
+    id = document.getElementById('idField').value;
     document.getElementById("registration").style.display = "none";
     document.getElementById("test").style.display = "block";
     document.getElementById("participantID").innerHTML = id;
-
 }
 
-var date = new Date();
-var count = 0;
-var arr = [];
-var pressTime;
-var jumpedTime;
-var reactTime;
-var correctness;
-var csvArr = [];
+function send_csv() {
+    alert("Sorry, this function is not available at the moment.")
+}
+/* Create 42 dots */
 var deg = 360 / 42;
 var radius = 200;
 var parentdiv = document.getElementById('parentdiv');
@@ -39,88 +34,84 @@ for (var i = 1; i <= 42; ++i) {
     childdiv.style.left = (x + totalOffset).toString() + "px";
     parentdiv.appendChild(childdiv);
 }
-// get childNOdes 
-c = document.getElementById("parentdiv").childNodes;
+c = document.getElementById("parentdiv").childNodes; // get childNOdes 
+c[42].style.backgroundColor = "green"; //starting indicator
 
-// onkeyup event. Start and react when there is a jump.
+
+/* Onkeyup event. Start and react when there is a jump.*/
+var count = 0;
+var arr = [];
+var pressTime, jumpedTime, reactTime, correctness;
+var csvArr = [];
+
 document.body.onkeyup = function(e) {
-    if (e.keyCode == 83) { //s
-        var fifteenMinutes = 60 * 3,
-            display = document.querySelector('#time');
-        startTimer(fifteenMinutes, display);
-        document.getElementById("dashboard").style.display = "block";
-    }
-    if (e.keyCode == 32) { //space bar
-        pressTime = Date.now();
-
-        reactTime = (pressTime - jumpedTime) / 1000;
-        document.getElementById("reactT").textContent = reactTime;
-        if (!isNaN(reactTime)) {
-            document.getElementById("correct").textContent = "Correct!";
-            correctness = "Correct";
-        } else {
-            document.getElementById("correct").textContent = "Incorrect!";
-            correctness = "Incorrect";
+        if (e.keyCode == 83) { //s
+            var fifteenMinutes = 60 * 15,
+                display = document.querySelector('#time');
+            startTimer(fifteenMinutes, display);
         }
-        csvArr.push([id, Date(pressTime), dot_pos_x, dot_pos_y, reactTime, correctness]);
-        jumpedTime = undefined;
-
+        if (e.keyCode == 32) { //space bar
+            pressTime = Date.now();
+            reactTime = (pressTime - jumpedTime) / 1000;
+            if (csvArr[csvArr.length - 1][2] == count - 1) { // when the normal movement has been recorded, but reacted, it's false positive.
+                correctness = "False Positive";
+                csvArr.push([id, Date(), count - 1, dot_pos_x.toFixed(3), dot_pos_y.toFixed(3), "NaN", correctness]);
+            } else {
+                correctness = "Correct";
+                csvArr.push([id, Date(pressTime), jump_idx, jump_pos_x.toFixed(3), jump_pos_y.toFixed(3), reactTime, correctness]);
+                jump_idx = undefined;
+            }
+        }
     }
-}
-
-// download_csv, with the final calculation of average reation time and correct rate.
-var avgReaction;
-var correctRate;
+    /* download_csv, with the final calculation of average reation time and correct rate. */
+var avgReaction, correctRate;
 var tempSum = 0;
 var validCount = 0;
 var correctCount = 0;
-var dot_pos_x, dot_pos_y;
 
 function download_csv() {
-    var csv = 'ID, Timestamp, Dot x position, Dot y position, React Time(s), Correctness\n';
+    var csv = 'ID, Timestamp, Dot Index, Dot x position, Dot y position, React Time(s), Correctness\n';
     csvArr.forEach(function(row) {
         csv += row.join(',');
         csv += "\n";
     });
-
     for (var i = 0; i < csvArr.length; i++) {
-        if (!isNaN(csvArr[i][4])) {
-            tempSum = csvArr[i][4] + tempSum;
+        if (!isNaN(csvArr[i][5])) {
+            tempSum = csvArr[i][5] + tempSum;
             validCount++;
         }
-        if (csvArr[i][5] == "Correct") correctCount++;
+        if (csvArr[i][6] == "Correct") correctCount++;
     }
     avgReaction = (tempSum / validCount).toFixed(2);
     correctRate = (correctCount / csvArr.length).toFixed(2);
-
-    finalArr = [' average reaction time (s): ', avgReaction, 'correct rate: ', correctRate];
+    finalArr = [',  average reaction time (s): ', avgReaction, 'correct rate: ', correctRate];
     csv += finalArr.join(',');
 
     console.log(csv);
     var hiddenElement = document.createElement('a');
     hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
     hiddenElement.target = '_blank';
-    hiddenElement.download = 'report.csv';
+    hiddenElement.download = id + '_report.csv';
     hiddenElement.click();
 }
 
 
+/* timer start. A green dot will visit all the white dots, except for the 10% jumps.*/
+var jump_idx, jump_pos_x, jump_pos_y, dot_idx, dot_pos_x, dot_pos_y;
 
-// timer start. A green dot will visit all the white dots, except for the 10% jumps.
 function startTimer(duration, display) {
     var timer = duration,
         minutes, seconds;
-
     var myTimer = setInterval(function() {
-        if (count == 0) { //ignore the first child
+        c[42].style.backgroundColor = "white"; //hide the starting indicator
+        if (count == 0) { //ignore the fixation cross.
             count++;
         }
         if (--timer <= 0) {
             document.getElementById("testDone").textContent = "You have accomplished this test!";
             document.getElementById("report").style.display = "block";
             document.getElementById("download_csv").style.display = "block";
-            document.getElementById("send_csv").style.display = "block";
-            document.getElementById("dashboard").style.display = "none";
+            document.getElementById("send_csv").style.display = "block"; // TODO. Github.io doesn't support hosting function of the server. 
             clearInterval(myTimer);
         }
         minutes = parseInt(timer / 60, 10);
@@ -128,47 +119,46 @@ function startTimer(duration, display) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         display.textContent = minutes + ":" + seconds;
-        // document.getElementById("childNodes").innerHTML = c.length;
-        // if 42 dots have been visited, regenerate a random number and continue
+
+        /* if 42 dots have been visited, regenerate random numbers and continue */
         if (count >= 43) {
             count = 1;
             arr = [];
             c[42].style.backgroundColor = "white";
         }
+        dot_pos_x = c[count].getBoundingClientRect().x;
+        dot_pos_y = c[count].getBoundingClientRect().y;
         if (count == 1) {
             while (arr.length < 42 * 0.1 - 1) {
                 var random_num = Math.floor(Math.random() * 41) + 1;
                 if (arr.indexOf(random_num) === -1) arr.push(random_num);
             }
-            // document.getElementById("myArr").innerHTML = arr;
-            if (arr.includes(1)) {
+        }
+        /* Perform a normal movement or a jump */
+        if (arr.includes(count)) { // Jumping cases
+            if (count != 1)
+                c[count - 1].style.backgroundColor = "white";
+            else if (count == 1) {
+                c[42].style.backgroundColor = "white";
+            }
+            if (count == 42) { c[1].style.backgroundColor = "green"; } else {
                 c[count + 1].style.backgroundColor = "green";
-                count++;
-            } else {
-                c[count].style.backgroundColor = "green";
             }
-        } else {
-            if (arr.includes(count)) {
-                //if the participant doesn't react on a jump before the next jump
-                // keep the record in log as "No Reaction"
-                if (csvArr.length > 1 && csvArr[csvArr.length - 1][2] != dot_pos_x) {
-                    csvArr.push([id, Date(), dot_pos_x, dot_pos_y, "NaN", "No Reaction"]);
-                }
-                c[count - 1].style.backgroundColor = "white";
-                if (count == 42) { c[1].style.backgroundColor = "green"; } else {
-                    c[count + 1].style.backgroundColor = "green";
-                }
-                jumpedTime = Date.now();
-                dot_pos_x = c[count].getBoundingClientRect().x;
-                dot_pos_y = c[count].getBoundingClientRect().y;
-                document.getElementById("dotpos").textContent = dot_pos_x + " , " + dot_pos_y;
-                // document.getElementById("jumpT").textContent = jumpedTime;
-                count++;
-            } else {
-                c[count - 1].style.backgroundColor = "white";
-                c[count].style.backgroundColor = "green";
+            jumpedTime = Date.now();
+            jump_idx = count;
+            jump_pos_x = c[count].getBoundingClientRect().x;
+            jump_pos_y = c[count].getBoundingClientRect().y;
+            count++; //count the jump
+        } else { // Normal cases
+            if (!isNaN(jump_idx) && csvArr.length > 1 && csvArr[csvArr.length - 1][2] != jump_idx) {
+                csvArr.push([id, Date(), jump_idx, jump_pos_x.toFixed(3), jump_pos_y.toFixed(3), "NaN", "No Reaction"]);
+                jump_idx = undefined;
             }
+            csvArr.push([id, Date(), count, dot_pos_x.toFixed(3), dot_pos_y.toFixed(3), "NaN", "Normal movement"]);
+
+            c[count - 1].style.backgroundColor = "white";
+            c[count].style.backgroundColor = "green";
         }
         count++;
-    }, 1000);
+    }, 1);
 }
